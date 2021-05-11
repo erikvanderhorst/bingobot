5c79fa80b62f7232cbc1a400b8295f19bab1f234
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-05-11 12:51:21 +0200

    added electro blog

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ $(function () {
         } else {
             $('.navigation').addClass("active");
         }
-
-
     });
+    // Same height 4 cards on homepage
+    var highestCard = 0;
+    $('.card img').each(function () {
+        if ($(this).height() > highestCard) {
+            highestCard = $(this).height() + 20;
+        }
+    });
+    $('.card img').height(highestCard);
 })
 
